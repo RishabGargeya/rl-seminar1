1171282282fcf415f1e0399bd2bb01e7a2eef373
Rishab Gargeya <rishab.gargeya@gmail.com>
2018-07-10 14:08:27 +0900

    Add files via upload

---
+++ cartpolev0.py
@@ -0,0 +1,19 @@
+import gym
+env = gym.make('CartPole-v0')
+env = gym.wrappers.Monitor(env, './vids/', video_callable= lambda episode_id:True ,force=True)
+env.reset()
+print(env.action_space)
+print(env.observation_space)
+for i_episode in range(20): # 20 trials
+    observation = env.reset()
+    for t in range(100): # maximum 100 iterations
+        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        print(observation, reward, done, info)
+
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+
+print("got here")
